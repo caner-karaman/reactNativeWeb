bc88bd73b33971c0c1c4b8bcaf261f57db71a313
Caner Karaman <karamancaner1995@gmail.com>
2021-12-27 19:02:31 +0300

    Add environments for android

--- App.tsx
+++ App.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-  Text,
-  View
-} from 'react-native';
-import styled from "styled-components";
+import {SafeAreaView, StatusBar, Text, View, NativeModules} from 'react-native';
+import styled from 'styled-components';
 
 const Wrapper = styled(View)`
-  background-color: red;
   align-items: center;
-`
+`;
 
 const App = () => {
+  const {env} = NativeModules.RNConfig;
+  console.log('env:');
+  console.log(env);
   return (
     <SafeAreaView>
-      <StatusBar barStyle='dark-content' />
+      <StatusBar barStyle="dark-content" />
       <Wrapper>
-        <Text style={{fontSize: 24}}>React Native Web App Example</Text>
+        <Text style={{fontSize: 24}}>Uygulama:</Text>
+        <Text style={{fontSize: 24}}>{env}</Text>
+        <Text style={{fontSize: 24}}>ortamında çalışmaktadır.</Text>
       </Wrapper>
     </SafeAreaView>
   );
